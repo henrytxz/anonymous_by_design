89b564dcbeba1104569c14227950caa0fab46e78
henrytxz <hzhang33@gmail.com>
2014-02-08 13:02:33 -0500

    1st try
    this outputs
    
    "This is one sentence"
    "Also one sentence!"
    
    but the correct result is
    ""	""	""	""	"This is one sentence"	""
    ""	""	""	""	"Also one sentence!"	""
    ""	""	""	""	"Three
    lines
    "	""
    
    the problem is split("\t") eliminates 
    ""	""	""	""	"Three
    lines
    "	""

--- lesson4/mapper.py
+++ lesson4/mapper.py
@@ -4,11 +4,20 @@ def mapper():
 
     count = 1
     
-    for line in reader:
-
+    #for line in reader:
+    for line in sys.stdin:
         # YOUR CODE HERE
             
-        if count ==3:    
-            writer.writerow(line)
-
-        count = count+1
+        #if count ==3:    
+        #writer.writerow(line)
+        #count = count+1
+        
+        data = line.strip().split("\t")
+        if len(data) == 6:
+            f1, f2, f3, f4, body, f6 = data
+            #print body.find(".")
+            minusLastChar = body.strip('"')[:-1]
+            #print body
+            punctuations = minusLastChar.count(".")+minusLastChar.count("!")+minusLastChar.count("?") 
+            if punctuations < 1:
+                print body
