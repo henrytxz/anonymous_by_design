69436f57d8674f8814293141bd0fa30ab1e7fbb1
henrytxz <hzhang33@gmail.com>
2014-02-15 14:03:01 -0500

    approved submission
    the pb with the last try was when one does 
    
    for line in sys.stdin:
    
    the /n char is already interpreted by the time you're in your iteration
    so
    if len(data) == 6:
    doesn't work, len(data) is 5 + 2, for instance
    
    for line in reader:
    however, does not interpret \n

--- lesson4/mapper.py
+++ lesson4/mapper.py
@@ -2,22 +2,11 @@ def mapper():
     reader = csv.reader(sys.stdin, delimiter='\t')
     writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
 
-    count = 1
-    
-    #for line in reader:
-    for line in sys.stdin:
-        # YOUR CODE HERE
-            
-        #if count ==3:    
-        #writer.writerow(line)
-        #count = count+1
+    for line in reader:
         
-        data = line.strip().split("\t")
-        if len(data) == 6:
-            f1, f2, f3, f4, body, f6 = data
-            #print body.find(".")
-            minusLastChar = body.strip('"')[:-1]
-            #print body
-            punctuations = minusLastChar.count(".")+minusLastChar.count("!")+minusLastChar.count("?") 
-            if punctuations < 1:
-                print body
+        # YOUR CODE HERE
+        body = line[4]
+        minusLastChar = body.strip('"')[:-1]
+        punctuations = minusLastChar.count(".")+minusLastChar.count("!")+minusLastChar.count("?") 
+        if punctuations < 1:
+            writer.writerow(line)
