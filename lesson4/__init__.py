0a4937a75af3fc1c04b0870bcbda35c6f5ef8d74
henrytxz <hzhang33@gmail.com>
2014-02-16 16:12:20 -0500

    Update __init__.py

--- lesson4/__init__.py
+++ lesson4/__init__.py
@@ -1,1 +1,5 @@
-#from strip_punctuation import strip_punctuation
+__init__.py in the same directory as the files tells Python that it's "ok to import from this directory".
+
+courtesy of http://stackoverflow.com/questions/4142151/python-how-to-import-the-class-within-the-same-directory-or-sub-directory
+
+
