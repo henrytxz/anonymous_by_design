9d0f9231b6b120f05b53bd4d9858510c81d6213a
henrytxz <hzhang33@gmail.com>
2014-02-16 13:21:26 -0500

    Create inverted_index_mapper.py

---
+++ lesson4/inverted_index_mapper.py
@@ -0,0 +1,14 @@
+def mapper():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+
+    for line in reader:
+        
+        # YOUR CODE HERE
+        body = line[4]
+        print body
+        
+        minusLastChar = body.strip('"')[:-1]
+        punctuations = minusLastChar.count(".")+minusLastChar.count("!")+minusLastChar.count("?") 
+        if punctuations < 1:
+        #    writer.writerow(line)
