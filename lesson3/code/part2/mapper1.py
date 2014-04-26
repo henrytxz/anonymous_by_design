7bcae224455f3c4dd2cb44213d454221b5e4e91c
henry <hzhang33@gmail.com>
2014-04-26 11:19:17 -0400

    bit of a cleanup

--- lesson3/code/part2/mapper1.py
+++ lesson3/code/part2/mapper1.py
@@ -4,7 +4,6 @@ import sys
 
 for line in sys.stdin:
     data = line.strip().split()
-    #print len(data)
     if len(data) == 10:
         IP,id,username,time,timeZone,method,page,protocol,status,objectSize = data
         print "{0}".format(page)
